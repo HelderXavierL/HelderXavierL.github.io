65faeee0067da24aa88d73fc56387b61b1c16558
HELDER CARVALHO XAVIER <helderxavier10@gmail.com>
2022-10-28 09:27:36 -0300

    correção animação

--- static/script.js
+++ static/script.js
@@ -28,18 +28,18 @@ navItem.forEach(item =>{
 const item = document.querySelectorAll("[data-anime]");
 
 const animeScroll = () => {
-    const windowTop = window.pageYOffset +window.innerHeight * 0.85;
+    const windowTop = window.pageYOffset + window.innerHeight * 0.85 ;
     
     item.forEach((element) => {
         if (windowTop > element.offsetTop) {
-            element.classList.add("animete");
+            element.classList.add("animate");
         } else {
             element.classList.remove("animate");
         }
     });
 }
 
-animeScroll()
+animeScroll();
 
 window.addEventListener("scroll", ()=>{
     animeScroll();
